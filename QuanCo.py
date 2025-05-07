7574b04203b927a5069dab04bee9fc7183f01aa4
NguyenThanhTrung-N2T <23521683@gm.uit.edu.vn>
2025-05-07 22:54:50 +0700

    add class NuocDi and QuanCo

--- QuanCo.py
+++ QuanCo.py
@@ -3,3 +3,6 @@ class QuanCoo:
         self.loai = loai    # pawn, rook, knight, bishop, queen, king
         self.mau = mau      # white hoặc black
         self.da_di = False  # đã từng di chuyển (phục vụ nhập thành, en passant)
+        
+    def __repr__(self):
+        return f"QuanCoo(loai={self.loai}, mau={self.mau}, da_di={self.da_di})"
